b9721273b7fa0a4c6c60813a84d9550a9606bb68
dishap22 <dishapant22@gmail.com>
2024-04-18 11:45:07 +0530

    Disable raw mode at exit

--- kilo.c
+++ kilo.c
@@ -1,11 +1,18 @@
+#include <stdlib.h> // atexit()
 #include <termios.h> // struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH
 #include <unistd.h> // read(), STDIN_FILENO
 
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-  struct termios raw;
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
 
-  tcgetattr(STDIN_FILENO, &raw);
-
+  struct termios raw = orig_termios;
   raw.c_lflag &= ~(ECHO); //prints each key typed to terminal in canonical mode, turned off here for raw mode (also used while typing pass in terminal)
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
