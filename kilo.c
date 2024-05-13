04f5657409864cb81248967894990ad782e0380b
dishap22 <dishapant22@gmail.com>
2024-05-13 19:06:30 +0530

    Display Keypresses & Update Control Signals

--- kilo.c
+++ kilo.c
@@ -1,5 +1,7 @@
+#include <ctype.h> // iscntrl()
+#include <stdio.h> // printf()
 #include <stdlib.h> // atexit()
-#include <termios.h> // struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH, ICANON
+#include <termios.h> // struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH, ICANON, ISIG, IXON, IEXTEN, ICRNL
 #include <unistd.h> // read(), STDIN_FILENO
 
 struct termios orig_termios; // store original attributes
@@ -13,12 +15,19 @@ void enableRawMode() {
   atexit(disableRawMode); 
  
   struct termios raw = orig_termios; // copy to current settings
-  raw.c_lflag &= ~(ECHO | ICANON); 
+  raw.c_iflag &= ~(ICRNL | IXON);
+  // Ctrl+S stops data from being transmitted to terminal, produces XOFF control character
+  // Ctrl+Q resumes data transmission, produces XON control character
+  // IXON turns these off, is an input flag, hence the I. ISIG, ICANON, IEXTEN are exceptions, not input flags but start with I
+  // ICRNL leads to any carriage return (Ctrl+M) being convered into newline characters. Disabling this. CR = Carriage Return, NL = Newline. 
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); 
   // ECHO flag prints each key typed to terminal turned off here for raw mode (also used while typing pass in terminal)
   // flag stored as 00000000000000000000000000001000 in binary
   // ~ sets value to 11111111111111111111111111110111
   // & sets value to retain original values in each position except 4th bit becoems 0 (turn off ECHO)
   // ICANON flag reads char by char instead of line by line
+  // ISIG turns off SIGINT signal sent by Ctrl+C and SIGTSTP signal sent by Ctrl+Z
+  // IEXTEN disables Ctrl+V setting that waits for another character to be typed and then sends this character
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // apply changes
 }
 
@@ -26,6 +35,16 @@ int main() {
   enableRawMode();
 
   char c;
-  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); //reads input character by character till q is pressed
+  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') //reads input character by character till q is pressed
+  {
+    if (iscntrl(c)) // checks if a character is a non-printable control character
+    {
+      printf("%d\n", c);
+    }
+    else
+    {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
