115301aa5565cd4b72e082b0559ec779b0c7dd05
dishap22 <dishapant22@gmail.com>
2024-05-13 18:13:15 +0530

    Turn off canonical mode

--- kilo.c
+++ kilo.c
@@ -1,21 +1,25 @@
 #include <stdlib.h> // atexit()
-#include <termios.h> // struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH
+#include <termios.h> // struct termios, tcgetattr(), tcsetattr(), ECHO, TCSAFLUSH, ICANON
 #include <unistd.h> // read(), STDIN_FILENO
 
-struct termios orig_termios;
+struct termios orig_termios; // store original attributes
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // reset to original settings
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
-
-  struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO); //prints each key typed to terminal in canonical mode, turned off here for raw mode (also used while typing pass in terminal)
-
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  tcgetattr(STDIN_FILENO, &orig_termios); // get original settings
+  atexit(disableRawMode); 
+ 
+  struct termios raw = orig_termios; // copy to current settings
+  raw.c_lflag &= ~(ECHO | ICANON); 
+  // ECHO flag prints each key typed to terminal turned off here for raw mode (also used while typing pass in terminal)
+  // flag stored as 00000000000000000000000000001000 in binary
+  // ~ sets value to 11111111111111111111111111110111
+  // & sets value to retain original values in each position except 4th bit becoems 0 (turn off ECHO)
+  // ICANON flag reads char by char instead of line by line
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // apply changes
 }
 
 int main() {
