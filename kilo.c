c0ddae4b266fdf0556693333d9ea37a9fe9151dc
dishap22 <dishapant22@gmail.com>
2024-05-15 08:30:27 +0530

    Finish Chapter 3

--- kilo.c
+++ kilo.c
@@ -19,7 +19,12 @@ enum editorKey
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** data ***/
@@ -105,14 +110,45 @@ int editorReadKey()
 
     if (seq[0] == '[') 
     {
-      switch (seq[1]) 
+      if (seq[1] >= '0' && seq[1] <= '9')
       {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT; // mapping arrow keys to wasd keys
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~')
+        {
+          switch (seq[1])
+          {
+            case '1': return HOME_KEY; 
+            case '3': return DEL_KEY; // delete key sends <esc>[3~
+            case '4': return END_KEY;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
+          }
+        }
+      }
+      else
+      {
+        switch (seq[1]) 
+        {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT; // mapping arrow keys to wasd keys
+          case 'H': return HOME_KEY; // Home key could be sent as <esc>[1~, <esc>[7~, <esc>[H, or <esc>OH
+          case 'F': return END_KEY; // End key could be sent as <esc>[4~, <esc>[8~, <esc>[F, or <esc>OF
+        }
       }
     }
+    else if (seq[0] == 'O') 
+    {
+      switch (seq[1]) 
+      {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
+      }
+    }
+
     return '\x1b';
   }
   else
@@ -287,6 +323,23 @@ void editorProcessKeypress()
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+
+    case END_KEY:
+      E.cx = E.screencols - 1;
+      break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--)
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+      break;  
     
     case ARROW_UP:
     case ARROW_DOWN:
