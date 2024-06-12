4398319fe1c14cea2a22c344bdcd7f462c88ecc3
dishap22 <dishapant22@gmail.com>
2024-06-12 18:52:12 +0530

    Allow Horizontal File Scrolling

--- kilo.c
+++ kilo.c
@@ -42,6 +42,7 @@ struct editorConfig
 {
   int cx, cy;
   int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -269,6 +270,13 @@ void editorScroll() {
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+  if (E.cx < E.coloff)
+  {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
+  }
 }
 
 void editorDrawRows(struct abuf *ab)
@@ -300,9 +308,10 @@ void editorDrawRows(struct abuf *ab)
     } 
     else 
     {
-      int len = E.row[filerow].size;
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[filerow].chars, len);
+      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -336,7 +345,7 @@ void editorRefreshScreen()
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6); // show cursor after printing
@@ -358,10 +367,7 @@ void editorMoveCursor(int key)
     }
     break;
   case ARROW_RIGHT:
-    if (E.cx != E.screencols - 1) 
-    {
-      E.cx++;
-    }
+    E.cx++;
     break;
   case ARROW_UP:
     if (E.cy != 0) 
@@ -423,6 +429,7 @@ void initEditor()
   E.cx = 0;
   E.cy = 0;
   E.rowoff = 0; // by default, scrolled to top of file
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
